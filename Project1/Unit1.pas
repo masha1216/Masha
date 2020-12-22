a4ff932009e56037478f88567318da282028c3df
name000076 <76463674+name000076@users.noreply.github.com>
2020-12-22 07:32:54 +0300

    Add files via upload

---
+++ Project1/Unit1.pas
@@ -0,0 +1,22 @@
+﻿Unit Unit1;
+
+interface
+
+uses System, System.Drawing, System.Windows.Forms;
+
+type
+  Form1 = class(Form)
+  {$region FormDesigner}
+  private
+    {$include Unit1.Form1.inc}
+  {$endregion FormDesigner}
+  public
+    constructor;
+    begin
+      InitializeComponent;
+    end;
+  end;
+
+implementation
+
+end.
